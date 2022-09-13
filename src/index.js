7ce09485db28fb598d9fd57b0a71a6d12da099a4
Patrick Barabe <pbarabe@arizona.edu>
2022-09-13 13:57:46 -0700

    Add data-uri images and built files to project
    Makes the Grad Lockup logo importable as data-uri strings and
    seeds the project's 'dist' folder with an initial build.
    
    references #1

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import { ArizonaBanner } from './arizonabanner'
 import { Footer } from './footer'
 import { Logo } from './logo'
+import { GradLockupBlue, GradLockupFullBlue, GradLockupFullWhite } from './img/images'
 
 export {
   ArizonaBanner,
   Footer,
+  GradLockupBlue,
+  GradLockupFullBlue,
+  GradLockupFullWhite,
   Logo
 }
