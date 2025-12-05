5318f7db9c7e585008c0c6cc1ee9e8041545292f
Ratish Shrestha <ratish@arizona.edu>
2025-12-05 11:02:28 -0700

    Add AZGrad CSE search box and Global Footer links to footer element
    - Use footer-links.json file to get all the footer links
    - Replace old CSS classnames with AZ Bootstrap 5 classnames

--- src/footer.js
+++ src/footer.js
@@ -4,83 +4,122 @@ import links from './footer-links.json'
 import './style.css'
 
 export function Footer () {
-  const linksColumn1 = links.columns[0].map((next, i) => {
-    return (
-      <li key={i}>
-        <a href={next.url} className="link-underline link-underline-opacity-0 link-underline-opacity-75-hover">{next.text}</a>
-      </li>
-    )
+  const essentialLinks = links.essentialLinks.map((item) => {
+    return item.map((link, i) => {
+      return (
+        <li key={i} className="nav-item">
+          <a href={link.url} className="bg-transparent nav-link">{link.text}</a>
+        </li>
+      )
+    })
   })
 
-  const linksColumn2 = links.columns[1].map((next, i) => {
-    return (
-      <li key={i}>
-        <a href={next.url} className="link-underline link-underline-opacity-0 link-underline-opacity-75-hover">{next.text}</a>
-      </li>
-    )
+  const informationForLinks = links.informationFor.map((item) => {
+    return item.map((link, i) => {
+      return (
+        <li key={i} className="d-flex px-0 mb-1 mb-md-2 nav-item">
+          <a href={link.url} className="bg-transparent nav-link">{link.text}</a>
+        </li>
+      )
+    })
+  })
+
+  const topicsLinks = links.topics.map((item) => {
+    return item.map((link, i) => {
+      return (
+        <li key={i} className="d-flex px-0 mb-1 mb-md-2 nav-item">
+          <a href={link.url} className="bg-transparent nav-link">{link.text}</a>
+        </li>
+      )
+    })
+  })
+
+  const resourcesLinks = links.resources.map((item) => {
+    return item.map((link, i) => {
+      return (
+        <li key={i} className="d-flex px-0 mb-1 mb-md-2 nav-item">
+          <a href={link.url} className="bg-transparent nav-link">{link.text}</a>
+        </li>
+      )
+    })
+  })
+
+  const socialMedia = links.socialMedia.map((item) => {
+    return item.map((link, i) => {
+      return (
+        <li key={i} className="d-flex px-0 mb-1 mb-md-2 nav-item">
+          <a href={link.url} className={link.icon + ' icon-link bg-transparent nav-link'}>{link.text}</a>
+        </li>
+      )
+    })
   })
 
   const year = new Date().getFullYear()
   return (
-    <footer id="footer" className="footer py-3 text-bg-warm-gray text-center">
+    <footer id="footer" className="footer py-3 text-bg-warm-gray">
       <div className="container py-4">
-        <div className="row align-top">
-          <div className="col-sm-3 text-left">
+        <div className="row align-top d-flex justify-content-between">
+          <div className="col-12 col-sm-5 col-md-4 col-lg-4">
             <a href="https://grad.arizona.edu/">
               <Logo
-                className="img-fluid center-block"
+                className="img-fluid"
                 variant="full-blue"
               />
             </a>
           </div>
-
-          <div className="col-sm-3 text-center">
-            <ul className="list-unstyled">
-              {linksColumn1}
-            </ul>
-          </div>
-
-          <div className="col-sm-3 text-center">
-            <ul className="list-unstyled">
-              {linksColumn2}
-            </ul>
-          </div>
-
-          <div className="col-sm-3 text-end">
-            <SearchForm />
-
-            <ul className="list-inline mt-3">
-              <li className="d-inline-block fs-3 p-1">
-                <a
-                  href="https://www.instagram.com/uagradcollege"
-                  className="link-underline link-underline-opacity-0 link-underline-opacity-75-hover"
-                >
-                  <i className="az-icon-instagram"></i>
-                  <span className="visually-hidden-focusable">Instagram</span>
-                </a>
-              </li>
-              <li className="d-inline-block fs-3 p-1">
-                <a
-                  href="https://www.linkedin.com/school/university-of-arizona-graduate-college/"
-                  className="link-underline link-underline-opacity-0 link-underline-opacity-75-hover"
-                >
-                  <i className="az-icon-linkedin"></i>
-                  <span className="visually-hidden-focusable">LinkedIn</span>
-                </a>
-              </li>
-            </ul>
+          <div className="col-12 col-sm-7 col-md-8 col-lg-8">
+            <div className="row d-flex justify-content-center justify-content-sm-end">
+              <div className="col-md-6">
+                <SearchForm />
+              </div>
+              <div className="col-12">
+                <ul className="nav-utility d-flex justify-content-center justify-content-sm-end clearfix nav">
+                  {essentialLinks}
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
 
         <hr />
 
-        <div className="row text-center">
-          <div className="col-12">
-            <i>
+        <div className="row">
+          <div className="col">
+            <div className="row">
+              <nav role="navigation" aria-labelledby="block-az-barrio-footer-menu-info-menu" id="block-az-barrio-footer-menu-info" className="col-12 col-sm-6 col-md-3">
+                <h2 className="h6 mt-md-3 text-uppercase" id="block-az-barrio-footer-menu-info-menu">Information For</h2>
+                <ul className="nav flex-column nav-utility clearfix">
+                  {informationForLinks}
+                </ul>
+              </nav>
+              <nav role="navigation" aria-labelledby="block-az-barrio-footer-menu-info-menu" id="block-az-barrio-footer-menu-info" className="col-12 col-sm-6 col-md-5">
+                <h2 className="h6 mt-md-3 text-uppercase" id="block-az-barrio-footer-menu-info-menu">Topics</h2>
+                <ul className="split-md-2-col nav flex-column nav-utility clearfix">
+                  {topicsLinks}
+                </ul>
+              </nav>
+              <nav role="navigation" aria-labelledby="block-az-barrio-footer-menu-info-menu" id="block-az-barrio-footer-menu-info" className="col-12 col-sm-6 col-md-2">
+                <h2 className="h6 mt-md-3 text-uppercase" id="block-az-barrio-footer-menu-info-menu">Resources</h2>
+                <ul className="nav flex-column nav-utility clearfix">
+                  {resourcesLinks}
+                </ul>
+              </nav>
+              <nav role="navigation" aria-labelledby="block-az-barrio-footer-menu-social-media-menu" id="block-az-barrio-footer-menu-social-media" className="col-12 col-sm-6 col-md-2">
+                <h2 className="h6 mt-md-3 text-uppercase" id="block-az-barrio-footer-menu-social-media-menu">Connect</h2>
+                <ul className="nav flex-column nav-utility clearfix">
+                  {socialMedia}
+                </ul>
+              </nav>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col text-center fw-light">
+            <em>
               We respectfully acknowledge <a href="https://www.arizona.edu/university-arizona-land-acknowledgment" target="_blank" rel="noreferrer" className="link-underline link-underline-opacity-0 link-underline-opacity-75-hover">the University of Arizona is on the land and territories of Indigenous peoples</a>.
               Today, Arizona is home to 22 federally recognized tribes, with Tucson being home to the O’odham and the Yaqui.
               The University strives to build sustainable relationships with sovereign Native Nations and Indigenous communities through education offerings, partnerships, and community service.
-            </i>
+            </em>
           </div>
         </div>
 
@@ -88,7 +127,7 @@ export function Footer () {
 
         <div className="row">
           <div className="col-6 text-start align-bottom">
-            © {year} Arizona Board of Regents on behalf of the University of Arizona
+            © {year} Arizona Board of Regents on behalf of <a href="https://www.arizona.edu/">The University of Arizona</a>
           </div>
           <div className="col-6 text-end">
             <a className="ua-privacy link-underline link-underline-opacity-0 link-underline-opacity-75-hover" rel="external" href="https://www.arizona.edu/information-security-privacy">University Information Security &amp; Privacy</a>
